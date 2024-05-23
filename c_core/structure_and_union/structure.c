c540dc412abe1954b91a0903d1a7ce6c8d210e9d
Witzcode0 <brijesh.gondaliya07@gmail.com>
2024-05-23 16:52:35 +0530

    structur and union

---
+++ c_core/structure_and_union/structure.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+struct book {
+    char name[20];
+    int page;
+    float price;
+};
+
+int main() {
+    struct book b1, b2;
+    
+    strcpy(b1.name, "Python");
+    b1.price = 300.24;
+    b1.page = 600;
+    
+    printf("Book name : %s\n", b1.name);
+    printf("Book price : %f\n", b1.price);
+    printf("Book pages : %d\n", b1.page);
+    
+    strcpy(b2.name, "JAVA");
+    b2.price = 500.24;
+    b2.page = 700;
+    
+    printf("Book name : %s\n", b2.name);
+    printf("Book price : %f\n", b2.price);
+    printf("Book pages : %d\n", b2.page);
+    return 0;
+}
